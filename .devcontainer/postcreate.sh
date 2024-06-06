08492cdc1a1ab1aa9f29d5699bfd1ee2c11d2de2
Lars Kruse <lakruzz@inc-inc.dk>
2024-06-06 07:49:29 +0000

    added a Pipfile and a script that runs it

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -2,4 +2,10 @@
 
 git config --local --get include.path | grep -e ../.gitconfig || git config --local --add include.path ../.gitconfig
 
-[ -f "$(git rev-parse --show-toplevel)/requirements.txt" ] && pip3 install --user -r "$(git rev-parse --show-toplevel)/requirements.txt" || echo no "requirements.txt" file
+pushd "$(git rev-parse --show-toplevel)" || exit 1
+if [ -f "Pipfile" ];then
+  pipenv install && pipenv shell|| echo no "Pipfile" file
+elif [ -f "requirements.txt" ];then 
+  pip3 install --user -r requirements.txt || echo No "requirements.txt" file
+fi
+popd || exit 1 
